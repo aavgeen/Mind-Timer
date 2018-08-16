7e6a0084529555925f4effafc2ef6dfbf5aea563
aavgeen1 <aavgeensingh1@gmail.com>
2018-08-17 00:06:01 +0530

    Added animations flexbox for timer

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <div className="meditationModal">
+          dwiwoiiowjdiowjdo
+        </div> */}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="header-logo">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Mind Timer</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-content">
+          <div className="mind-timer">
+            <div style={{fontSize: 'xx-large'}}>
+              <span className="breatheIn">Breathe In</span><span className="andOut"> &amp; out.</span>
+              <div className="duration">
+                 Duration
+              </div>
+              <div className="sounds">
+                <div className="starting-sound indisounds">
+                  Starting Sound
+                </div>
+                <div className="ambientSound indisounds">
+                 Ambient Sounds
+                </div>
+                <div className="ending-sound indisounds">
+                  Ending Sound
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
